6b366eb7c48f84378aa9079134ebf541251aa6b3
Mihail-Madzharov <mihail.madzharov.m@gmail.com>
2019-06-09 13:48:14 +0300

    Game:Create game board component

--- src/app/home/game/game-board/game-board.component.spec.ts
+++ src/app/home/game/game-board/game-board.component.spec.ts
@@ -1,21 +1,20 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GameComponent } from "./game.component";
+import { GameBoardComponent } from "./game-board.component";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-describe("GameComponent", () => {
-  let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
+describe("GameBoardComponent", () => {
+  let component: GameBoardComponent;
+  let fixture: ComponentFixture<GameBoardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [GameComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      declarations: [GameBoardComponent]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GameComponent);
+    fixture = TestBed.createComponent(GameBoardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { GameComponent } from "./game/game.component";
 import { HomeComponent } from "./home.component";
+import { GameBoardComponent } from "./game/game-board/game-board.component";
 
 @NgModule({
-  declarations: [GameComponent, HomeComponent],
+  declarations: [GameComponent, HomeComponent, GameBoardComponent],
   imports: [CommonModule]
 })
 export class HomeModule {}
