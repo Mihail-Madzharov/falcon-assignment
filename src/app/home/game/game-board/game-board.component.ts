f43e0f488093022d221a7bac7e7c47152f0ad74f
Mihail-Madzharov <mihail.madzharov.m@gmail.com>
2019-06-09 14:43:50 +0300

    Game: generate matrix and select index

--- src/app/home/game/game-board/game-board.component.ts
+++ src/app/home/game/game-board/game-board.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { generateMatrixModel, Matrix } from "src/app/lib/game-utilities/matrix";
 
 @Component({
-  selector: 'app-game-board',
-  templateUrl: './game-board.component.html',
-  styleUrls: ['./game-board.component.scss']
+  selector: "app-game-board",
+  templateUrl: "./game-board.component.html",
+  styleUrls: ["./game-board.component.scss"]
 })
 export class GameBoardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public gameBoard: Matrix;
+  constructor() {
+    this.gameBoard = generateMatrixModel(7, 6);
   }
 
+  ngOnInit() {}
+
+  onClickHandler(row) {
+    const col = this.gameBoard[row].findIndex(value => value === 0);
+    this.gameBoard[row][col] = 1;
+  }
 }
