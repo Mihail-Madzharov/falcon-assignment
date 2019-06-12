c96c9e3d8a9208cd84f9efe07535c6cf2d0e8d59
mihail madzharov <mihail.madzharov.m@gmail.com>
2019-06-12 20:07:10 +0300

    Testing: Reducers

--- src/app/home/game/store/game.reducers.spec.ts
+++ src/app/home/game/store/game.reducers.spec.ts
@@ -4,19 +4,24 @@ import {
   UpdateGameBoardAction,
   UpdateCurrentUserIdAction,
   ToggleGameStartAction,
-  UpdateLastPlayingPlayer
+  UpdateLastPlayingPlayer,
+  StartNewGame,
+  UpdateWinnerIdAction,
+  ResetGameStateAction
 } from "./game.actions";
 import { Matrix } from "src/app/lib/game-utilities/matrix";
+import { PlayersEnum } from "../players/players.enum";
 
 describe("Gamer reducer", () => {
   let gameState: GameState;
   beforeEach(() => {
     gameState = {
       gameBoard: [],
-      currentUserId: 1,
-      secondUserId: 1,
+      currentUserId: PlayersEnum.NoPlayerSelectedId,
+      secondUserId: PlayersEnum.NoPlayerSelectedId,
       gameStarted: false,
-      lastPlayingPlayer: 1
+      lastPlayingPlayer: PlayersEnum.NoPlayerSelectedId,
+      winnerId: null
     };
   });
 
@@ -61,5 +66,44 @@ describe("Gamer reducer", () => {
 
     // Assert
     expect(result.lastPlayingPlayer).toBe(playerId);
+  });
+
+  it("should update the winnerId on UpdateWinnerIdAction", () => {
+    // Arrange
+    const playerId = 1;
+    const toggleGameStartAction = new UpdateWinnerIdAction(playerId);
+    // Act
+    const result = gameReducer(gameState, toggleGameStartAction);
+
+    // Assert
+    expect(result.winnerId).toBe(playerId);
+  });
+
+  it("should update reset the state on ResetGameState", () => {
+    // Arrange
+    const initialState = Object.assign({}, { ...gameState });
+    gameState.lastPlayingPlayer = 1;
+
+    // Act
+    const result = gameReducer(gameState, new ResetGameStateAction());
+
+    // Assert
+    expect(result).toEqual(initialState);
+  });
+
+  it(`should start a new game by preserving
+     the currentUserId the secondUserId and gameStarted to true`, () => {
+    // Arrange
+    const startNewGameAction = new StartNewGame();
+    gameState.currentUserId = 1;
+    gameState.secondUserId = 2;
+    gameState.gameStarted = true;
+    // Act
+    const result = gameReducer(gameState, startNewGameAction);
+
+    // Assert
+    expect(result.currentUserId).toBe(1);
+    expect(result.secondUserId).toBe(2);
+    expect(result.gameStarted).toBeTruthy();
   });
 });
